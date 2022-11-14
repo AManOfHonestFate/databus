200b205b947c60b631368a89596a5f92c5d15234
Egor K <khromov_dev@mail.ru>
2022-11-14 12:21:35 +0300

    check for number of arguments

--- databus_client/main.go
+++ databus_client/main.go
@@ -12,9 +12,12 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-
-
 func main() {
+	// requires 3 arguments: target, param1, param2
+	if len(os.Args) != 4 {
+		log.Fatalf("requires 3 arguments, given %v", len(os.Args) - 1)
+	}
+	
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(os.Args[1], grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -27,6 +30,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	
+	// Parsing params
 	prm1, err := strconv.ParseFloat(os.Args[2], 32)
 	if err != nil {
 		log.Fatalf("didn't parse param1: %v", err)
@@ -36,9 +40,10 @@ func main() {
 		log.Fatalf("didn't parse param2: %v", err)
 	}
 	
+	// RPC call
 	r, err := c.Send(ctx, &pb.SendRequest{Prm1: float32(prm1), Prm2: float32(prm2)})
 	if err != nil {
-		log.Fatalf("did not send: %v", err)
+		log.Fatalf("RPC call failed: %v", err)
 	}
 	log.Println(r.GetResult())
 }
