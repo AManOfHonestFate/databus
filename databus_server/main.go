9b4c4ddc14cf6c5fba6d27417e9231f553399f56
Egor K <khromov_dev@mail.ru>
2022-11-14 12:15:19 +0300

    refactor: comments

--- databus_server/main.go
+++ databus_server/main.go
@@ -17,32 +17,49 @@ type server struct {
 	pb.UnimplementedDatabusServiceServer
 }
 
-func (s* server) Send(ctx context.Context, in *pb.SendRequest) (*pb.SendResponse, error) {
-	a, b := in.GetPrm1(), in.GetPrm2()
-	var res float32
-
-	switch os.Args[2] {
+// does given operation on 2 floats
+func operation(op string, a float32, b float32) (float32, error) {
+	var res float32	
+	
+	switch op {
 		case "add":
 			res = a + b
-			fmt.Printf("%v + %v = %v", a, b, res)
+			fmt.Printf("%v + %v = %v\n", a, b, res)
 		case "sub":
 			res = a - b
-			fmt.Printf("%v - %v = %v", a, b, res)
+			fmt.Printf("%v - %v = %v\n", a, b, res)
 		case "mul":
 			res = a * b
-			fmt.Printf("%v * %v = %v", a, b, res)
+			fmt.Printf("%v * %v = %v\n", a, b, res)
 		case "div":
 			res = a / b
-			fmt.Printf("%v / %v = %v", a, b, res)
+			fmt.Printf("%v / %v = %v\n", a, b, res)
 		default:
-			fmt.Printf("wrong operation: %s", os.Args[2])
-			return nil, errors.New("Wrong operation")
+			return 0, errors.New("unsupported operation")
+	}
+
+	return res, nil
+}
+
+// Send implementation
+func (s* server) Send(ctx context.Context, in *pb.SendRequest) (*pb.SendResponse, error) {
+	a, b := in.GetPrm1(), in.GetPrm2()
+
+	res, err := operation(os.Args[2], a, b)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
 	}
 
 	return &pb.SendResponse{Result: res}, nil
 }
 
 func main() {
+	// requires 2 arguments: port, operation
+	if len(os.Args) != 3 {
+		log.Fatalf("requires 2 arguments, given %v", len(os.Args))
+	}
+	
 	// Listening on port given as the first argument
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", os.Args[1]))
 	if err != nil {
